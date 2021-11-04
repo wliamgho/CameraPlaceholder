00bbc126ede274420b78a0f09730253ae91ecc25
William <will.liiam06@gmail.com>
2021-11-04 12:42:25 +0700

    feat: initial project

---
+++ CameraPlaceholder/CameraPlaceholder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraPlaceholder
+//
+//  Created by William on 04/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
